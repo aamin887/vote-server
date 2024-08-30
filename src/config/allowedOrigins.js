54d4c73b3d863ab85837eca4b8cee74631d0c274
Aamin88 <forkahamin@yahoo.co.uk>
2024-08-30 16:31:28 +0100

    update: updating cors allowed list with live url for client

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -6,6 +6,7 @@ const allowedOrigin = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5500",
+  "https://vote-client.onrender.com",
 ];
 
 module.exports = allowedOrigin;
