96b3b99e89b6d7ad92f96dec37e5946e9c8f14fd
Aamin88 <forkahamin@yahoo.co.uk>
2024-09-28 12:30:50 +0100

    fix client url

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -6,7 +6,7 @@ const allowedOrigin = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5500",
-  "https://vote-client.onrender.com/login",
+  "https://vote-client.onrender.com",
   "https://voting-app-pi-seven.vercel.app",
 ];
 
